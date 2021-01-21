4a4374ffe0020cae54bf58bc4ac36bb8760c0fd8
jiangyingfeng <jiangyingfeng163@163.com>
2021-01-21 13:44:14 +0800

    提交文件

---
+++ main.cpp
@@ -0,0 +1,10 @@
+﻿#include <QApplication>
+#include <QMessageBox>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QMessageBox::information(NULL, "TEST", "测试窗口",QMessageBox::Yes|QMessageBox::No, QMessageBox::Yes);
+
+    return a.exec();
+}
